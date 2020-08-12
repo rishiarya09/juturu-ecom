a1f418257d8a86e828f8cb0116e32d40a8dafb00
rishiarya <rishairya09@gmail.com>
2020-08-12 17:17:23 -0300

    errors verified

--- includes/conn.php
+++ includes/conn.php
@@ -2,9 +2,9 @@
 
 Class Database{
  
-	private $server = "mysql:host=localhost;dbname=ecomm";
-	private $username = "root";
-	private $password = "";
+	private $server = "mysql:host=localhost;dbname=id10708634_juturu_v1";
+	private $username = "id10708634_juturu_admin";
+	private $password = "lFpNt|Wf*ox69W|N";
 	private $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,);
 	protected $conn;
  	
--- includes/header.php
+++ includes/header.php
@@ -7,18 +7,18 @@
   	<!-- Tell the browser to be responsive to screen width -->
   	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   	<!-- Bootstrap 3.3.7 -->
-  	<link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+  	<link rel="stylesheet" type="text/css" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
   	<!-- DataTables -->
-    <link rel="stylesheet" href="bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
   	<!-- Font Awesome -->
-  	<link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
+  	<link rel="stylesheet" type="text/css" href="bower_components/font-awesome/css/font-awesome.min.css">
   	<!-- Theme style -->
-  	<link rel="stylesheet" href="dist/css/AdminLTE.min.css">
+  	<link rel="stylesheet" type="text/css" href="dist/css/AdminLTE.min.css">
   	<!-- AdminLTE Skins. Choose a skin from the css/skins
        folder instead of downloading all of them to reduce the load. -->
-  	<link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
+  	<link rel="stylesheet" type="text/css" href="dist/css/skins/_all-skins.min.css">
     <!-- Magnify -->
-    <link rel="stylesheet" href="magnify/magnify.min.css">
+    <link rel="stylesheet" type="text/css"  href="magnify/magnify.min.css">
 
   	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -28,7 +28,7 @@
   	<![endif]-->
 
   	<!-- Google Font -->
-  	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+  	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 
     <!-- Paypal Express -->
     <script src="https://www.paypalobjects.com/api/checkout.js"></script>
