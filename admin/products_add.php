a9a4eb3c21473b2888a61f5fe3d750f92d247d5c
rishiarya <rishairya09@gmail.com>
2020-08-14 08:12:10 -0300

    image url

--- admin/products_add.php
+++ admin/products_add.php
@@ -54,7 +54,7 @@ try {
 				// move_uploaded_file($_FILES['photo']['tmp_name'][$i], '../images/'.$new_filename);	
 				// $file = $_FILES['photo']['tmp_name'][$i];
 				$keyName = 'test_example/' . basename($_FILES["photo"]['name'][$i]);
-				$pathInS3 = 'https://s3.us-east-2.amazonaws.com/' . $bucketName . '/' . $keyName;
+				$image_url = 'https://s3.us-east-2.amazonaws.com/' . $bucketName . '/' . $keyName;
 
 	// Add it to S3
 	try {
